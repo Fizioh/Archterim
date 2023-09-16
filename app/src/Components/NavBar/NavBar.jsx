b1c6fa05be85d5ff6b10d54d5d8f0e21362c1525
Fizioh <batrom@laposte.net>
2023-09-16 11:05:39 +0200

    feat: navbar complete

--- app/src/Components/NavBar/NavBar.jsx
+++ app/src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navBar flex justify-between items-center p-[3rem]'>
+      <div className='logoDiv'>
+        <h1 className='logo text-[25px] text-blueColor'>
+          <strong>Arch</strong>Terim
+        </h1>
+      </div>
+      <div className='menu flex gap-8'>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Offres</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Entreprises</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>A propos</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Contact</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Connexion</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Inscription</li>
+      </div>
+      
+    </div>
   )
 }
 
